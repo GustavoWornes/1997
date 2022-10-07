828b91dedce8836ef9ebfdce1b1baeac4b45ce5b
Gustavo <79329987+GustavoWornes@users.noreply.github.com>
2022-10-07 15:56:07 -0300

    Arrumando erro

--- pages/About/index.jsx
+++ pages/About/index.jsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 
 import Image from "next/image";
 import styles from "../../styles/Home.module.css"
-import container from "../About/about.module.css"
+import container from "../About/About.module.css"
 import teste from "../../public/eu.jpeg"
 export default function About (){
     return(
